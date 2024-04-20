9b22b7e9e93ee41f47fc7002e6a6ffd018085b68
Alexey Krutyakov <coder.arctik@gmail.com>
2024-04-20 14:55:38 +0300

    feat: add tests for PostsService

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -14,9 +14,21 @@ describe('PostsService', () => {
 
   it('should add a new post', () => {
     // реализуйте тест-кейс
+
+    expect(postsService.create(post)).toMatchObject({
+      id: expect.any(String),
+      text: post.text,
+      date: expect.any(String),
+    });
   });
 
   it('should find a post', () => {
     // реализуйте тест-кейс
+
+    expect(postsService.find('1')).toMatchObject({
+      id: '1',
+      text: 'Some pre-existing post',
+      date: expect.any(String),
+    });
   });
 });
